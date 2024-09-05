29d1c1c480c24a701f159112e9f6a6d0ba50b404
Ran <xyfwdy@gmail.com>
2024-09-05 14:37:40 +0800

    细分更多种类的漏斗 #1

--- src/main/java/cc/ranmc/hopper/Main.java
+++ src/main/java/cc/ranmc/hopper/Main.java
@@ -37,13 +37,12 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Main extends JavaPlugin implements Listener {
 
     private String prefix;
-    private YamlConfiguration data;
-    private File yml;
+    private YamlConfiguration dataYml, chunkYml;
+    private File dataFile, chunkFile;
     private List<String> lock = new ArrayList<>();
     private Map<String, Integer> hopperCount;
     private boolean enable;
     private int delay;
-    private String HopperName;
     private final boolean folia = isFolia();
     private BukkitTask task = null;
     private ScheduledTask foliaTask = null;
@@ -59,7 +58,6 @@ public class Main extends JavaPlugin implements Listener {
         print("&e-----------------------");
 
         loadConfig();
-        //updateCheck();
 
         if (folia) {
             foliaTask = Bukkit.getServer().getGlobalRegionScheduler().runAtFixedRate(this, task -> tickFolia(), 20, 20);
@@ -166,15 +164,15 @@ public class Main extends JavaPlugin implements Listener {
 
     private void hopperAddItem(Location location, Chunk chunk, String name) {
         lock.remove(name);
-        List<String> itemList = getConfig().getStringList("itemList");
-        if (!enable || !chunk.isLoaded() || data.getString(name) == null) return;
+        if (!enable || !chunk.isLoaded() || dataYml.getString(name) == null) return;
         Entity[] entities = chunk.getEntities();
-        String[] xyz = Objects.requireNonNull(data.getString(name)).split("x");
+        String[] xyz = Objects.requireNonNull(dataYml.getString(name)).split("x");
         Block block = location.getWorld().getBlockAt(Integer.parseInt(xyz[0]),
                 Integer.parseInt(xyz[1]),
                 Integer.parseInt(xyz[2]));
         if (block.getType() == Material.HOPPER) {
             Hopper hopper = (Hopper) block.getState();
+            List<String> itemList = chunkYml.getStringList(hopper.getCustomName());
             for (Entity entity : entities) {
                 if (entity.getType() == EntityType.DROPPED_ITEM) {
                     Item item = (Item) entity;
@@ -188,8 +186,10 @@ public class Main extends JavaPlugin implements Listener {
                 }
             }
         } else {
-            data.set(name, null);
-            try { data.save(yml); } catch (IOException ignore) {}
+            dataYml.set(name, null);
+            try {
+                dataYml.save(dataFile);
+            } catch (IOException ignore) {}
         }
     }
 
@@ -226,10 +226,10 @@ public class Main extends JavaPlugin implements Listener {
                 return;
             }
             Hopper hopper = (Hopper) block.getState();
-            String name = hopper.getWorld().getName()+hopper.getChunk().getX()+"x"+hopper.getChunk().getZ();
-            if (HopperName.equals(hopper.getCustomName())) {
-                if (data.getString(name) != null) {
-                    String[] xyz = Objects.requireNonNull(data.getString(name)).split("x");
+            String name = hopper.getWorld().getName()+hopper.getChunk().getX() + "x" + hopper.getChunk().getZ();
+            if (chunkYml.contains(hopper.getCustomName())) {
+                if (dataYml.getString(name) != null) {
+                    String[] xyz = Objects.requireNonNull(dataYml.getString(name)).split("x");
                     player.sendMessage(prefix + color("&c该区块已存在区块漏斗 x"+xyz[0]+" y"+xyz[1]+" z"+xyz[2]));
                     event.setCancelled(true);
                     return;
@@ -242,9 +242,9 @@ public class Main extends JavaPlugin implements Listener {
                 xyz.append(location.getBlockY());
                 xyz.append("x");
                 xyz.append(location.getBlockZ());
-                data.set(name,xyz.toString());
+                dataYml.set(name,xyz.toString());
                 try {
-                    data.save(yml);
+                    dataYml.save(dataFile);
                 } catch (IOException ignored) {}
             }
 
@@ -278,11 +278,11 @@ public class Main extends JavaPlugin implements Listener {
             Hopper hopper = (Hopper) block.getState();
             String chunkName = block.getChunk().toString();
             if (hopperCount.containsKey(chunkName)) hopperCount.put(chunkName, hopperCount.get(chunkName) - 1);
-            if (HopperName.equals(hopper.getCustomName())) {
+            if (chunkYml.contains(hopper.getCustomName())) {
                 player.sendMessage(prefix + color("&e你破坏了一个区块漏斗"));
-                data.set(hopper.getWorld().getName()+hopper.getChunk().getX()+"x"+hopper.getChunk().getZ(),null);
+                dataYml.set(hopper.getWorld().getName()+hopper.getChunk().getX()+"x"+hopper.getChunk().getZ(),null);
                 try {
-                    data.save(yml);
+                    dataYml.save(dataFile);
                 } catch (IOException ignore) {}
             }
 
@@ -294,7 +294,7 @@ public class Main extends JavaPlugin implements Listener {
      */
     @Override
     public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("ch") && args.length==1) {
+        if (cmd.getName().equalsIgnoreCase("ch") && args.length == 1) {
             if (args[0].equalsIgnoreCase("reload")) {
                 if (sender.hasPermission("ch.admin")) {
                     loadConfig();
@@ -323,7 +323,7 @@ public class Main extends JavaPlugin implements Listener {
      * 加载配置文件
      */
     private void loadConfig() {
-        //加载config
+        // 加载 Config
         if (!new File(getDataFolder() + File.separator + "config.yml").exists()) {
             saveDefaultConfig();
         }
@@ -331,16 +331,22 @@ public class Main extends JavaPlugin implements Listener {
 
         lock = new ArrayList<>();
 
-        //加载数据
-        yml = new File(this.getDataFolder(), "data.yml");
-        if (!yml.exists()) {
+        // 加载数据
+        chunkFile = new File(this.getDataFolder(), "chunk.yml");
+        if (!chunkFile.exists()) {
+            this.saveResource("chunk.yml", true);
+        }
+        chunkYml = YamlConfiguration.loadConfiguration(chunkFile);
+
+        dataFile = new File(this.getDataFolder(), "data.yml");
+        if (!dataFile.exists()) {
             this.saveResource("data.yml", true);
         }
-        data = YamlConfiguration.loadConfiguration(yml);
+        dataYml = YamlConfiguration.loadConfiguration(dataFile);
 
         enable = getConfig().getBoolean("enable",true);
-        delay = getConfig().getInt("delay",20);
-        HopperName = getConfig().getString("name","区块漏斗");
+        delay = getConfig().getInt("delay",30);
+
         hopperCount = new HashMap<>();
         prefix = color(getConfig().getString("prefix", "&b[区块漏斗]"));
     }
